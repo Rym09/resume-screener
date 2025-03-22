b2158f544bd4bd21f1ac53ca363b1861cc199be0
Rym09 <rym.challouf24@gmail.com>
2025-03-22 16:39:23 +0100

    Initial commit

---
+++ backend/main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"message": "Welcome to the Resue Screener API"}
